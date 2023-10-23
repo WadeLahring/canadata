f841770ea44eb023838dbcaa1680a73f6470648e
Wade Lahring <wadelahring@gmail.com>
2023-10-22 19:55:52 -0600

    Added real data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,154 +1,108 @@
-d3.csv("../data/data.csv").then(function(data) {
+// Define the dimensions and scales for the chart
+const margin = {top: 40, right: 20, bottom: 80, left: 40};
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
+
+// Create the SVG container
+const svg = d3.select("#chartContainer")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("class", "bg-white border p-4 mt-4")
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Load and parse the CSV data
+d3.csv("../data/data.csv").then(data => {
+    const parseYear = d3.timeParse("%Y");
+    
     data.forEach(d => {
-        d.year = new Date(d.year, 0);
-        d.seriesA = +d.seriesA;
-        d.seriesB = +d.seriesB;
+        d.Year = parseYear(d.Year);
     });
-    drawChart(data);
-}).catch(function(error) {
+    console.log(data)
+
+    // Create series from the data
+    const series = createSeries(data);
+
+    // Create chart with the series
+    drawChart(data, series);
+
+    }).catch(function(error) {
         console.error("Error loading the CSV file:", error);
 });
 
-function drawChart(data) {
+// This function transforms the data into series
+function createSeries(data) {
+    const seriesNames = data.columns.slice(1); // Exclude 'Year'
 
-// Define the dimensions and scales for the chart
-const width = 750;
-const height = 575;
-const margin = {top: 40, right: 20, bottom: 80, left: 40};
+    const series = seriesNames.map(name => {
+        return {
+            name: name,
+            values: data.map(d => ({
+                date: d.Year,
+                value: +d[name]
+            }))
+        };
+    });
 
-const xScale = d3.scaleTime()
-    .domain([new Date(2018, 0), new Date(2023, 0)])
-    .range([margin.left, width - margin.right]);
-
-const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => Math.max(d.seriesA, d.seriesB))])
-    .range([height - margin.bottom, margin.top]);
-
-// Create an SVG container
-const svg = d3.select("#chartContainer")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("class", "bg-white border");
-
-// Add the title
-svg.append("text")
-    .attr("x", margin.left)    
-    .attr("y", margin.top)
-    .attr("text-anchor", "start")
-    .attr("class", "chart-title font-poppins text-2x1")
-    .text("GDP per Capita (CAD)");
-
-// Add the axes
-const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
-const yAxis = d3.axisLeft(yScale);
-
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis cackle")
-    .attr("transform", `translate(${margin.left})`)
-    .call(yAxis);
-
-// Define the lines, one for each series
-const lineA = d3.line()
-    .x(d => xScale(d.year))
-    .y(d => yScale(d.seriesA));
-
-const lineB = d3.line()
-    .x(d => xScale(d.year))
-    .y(d => yScale(d.seriesB));
-
-// Add the lines, one for each series
-svg.append("path")
-    .datum(data)
-    .attr("class", "line lineA")
-    .attr("d", lineA)
-    .style("stroke", "steelblue")
-    .style("fill", "none");
-
-svg.append("path")
-    .datum(data)
-    .attr("class", "line lineB")
-    .attr("d", lineB)
-    .style("stroke", "darkorange")
-    .style("fill", "none");
-
-// Add dots for data points, one for each series
-svg.selectAll(".dotA")
-    .data(data)
-    .enter().append("circle")
-    .attr("class", "dotA")
-    .attr("cx", d => xScale(d.year))
-    .attr("cy", d => yScale(d.seriesA))
-    .attr("r", 3)
-    .style("fill", "steelblue");
-
-svg.selectAll(".dotB")
-    .data(data)
-    .enter().append("circle")
-    .attr("class", "dotB")
-    .attr("cx", d => xScale(d.year))
-    .attr("cy", d => yScale(d.seriesB))
-    .attr("r", 3)
-    .style("fill", "darkorange");
-
-// Define the Legend's properties
-const legendSpacing = 20;
-const legendHeight = 20;
-const legendWidth = 130;
-const legendOffset = 20;
-
-const seriesInfo = [
-    {name: "Alberta", color: "steelblue", visible: true},
-    {name: "British Columbia", color: "darkorange", visible: true}
-];
-
-// Create the legend
-const legend = svg.append("g")
-    .attr("class", "legend")
-    .attr("transform", `translate(${margin.left},${height - legendOffset})`);
-
-seriesInfo.forEach((series, i) => {
-    const legendGroup = legend.append("g")
-        .attr("transform", `translate(${i * (legendWidth + legendSpacing)}, 0)`)
-        .on("click", function() {
-            // Toggle series visibility
-            series.visible = !series.visible;
-
-            // Update chart
-            toggleSeries(series.name, series.visible);
-
-            // Update legend styling
-            d3.select(this).select("rect").style("opacity", series.visible ? 1 : 0.5);
-        });
-
-    // Rectangle for color
-    legendGroup.append("rect")
-        .attr("width", legendWidth)
-        .attr("height", legendHeight)
-        .style("fill", series.color);
-    
-    // Series name
-    legendGroup.append("text")
-        .attr("x", legendWidth / 2) // Centering the text inside the rectangle
-        .attr("y", legendHeight / 2) // Centering the text vertically the rectangle
-        .attr("dy", ".35em") // Adjust position to vertically center text
-        .style("text-anchor", "middle")
-        .text(series.name);
-});
-
-// Implement the toggleSeries function to show / hide series
-function toggleSeries(seriesName, isVisible) {
-    if (seriesName === "Alberta") {
-        svg.selectAll(".dotA").style("display", isVisible ? "initial" : "none");
-        svg.selectAll(".lineA").filter(d => d === data).style("display", isVisible ? "initial" : "none");
-    } else if (seriesName === "British Columbia") {
-        svg.selectAll(".dotB").style("display", isVisible ? "initial" : "none");
-        svg.selectAll(".lineB").filter(d => d === data).style("display", isVisible ? "initial" : "none");
-    }
+    return series;
 }
+
+function drawChart(data, series) {
+    // Compute scale domains
+    const xDomain = d3.extent(series[0].values, d => d.date);
+
+    const yDomain = [
+        d3.min(series, s => d3.min(s.values, d => d.value)),
+        d3.max(series, s => d3.max(s.values, d => d.value))
+    ];
+
+    // Define scales
+    const xScale = d3.scaleTime()
+        .domain(xDomain)
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+        .domain(yDomain)
+        .range([height, 0]);
+
+    // Create a color scale
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // Add the title
+    svg.append("text")
+        .attr("x", 0)
+        .attr("y", -20)
+        .attr("text-anchor", "start")
+        .attr("class", "chart-title font-poppins text-2x1")
+        .text("GDP per Capita (2022 CAD)");
+
+    // Add the axes
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    // Modify the line generator to handle missing points DOESNT WORK YET
+    const lineGenerator = d3.line()
+        .defined(d => d.value !== 0)
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.value));
+
+    // Plot each series
+    series.forEach((s, index) => {
+        svg.append("path")
+            .datum(s.values)
+            .attr("fill", "none")
+            .attr("stroke", colorScale(index))
+            .attr("stroke-width", 1.5)
+            .attr("d", lineGenerator);
+    });
 }
