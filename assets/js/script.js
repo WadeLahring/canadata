0206030c64bbb10a49d4b336643bd8d14be42f74
Wade Lahring <wadelahring@gmail.com>
2023-10-25 08:28:12 -0600

    Improved chart styling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // Define the dimensions and scales for the chart
-const margin = {top: 40, right: 20, bottom: 80, left: 40};
-const width = 800 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const margin = {top: 40, right: 80, bottom: 60, left: 60};
+const width = 700 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
-// Create the SVG container
+// Define the currency format
+const currencyFormat = d3.format("$,")
+
+// Create the SVG container for the chart
 const svg = d3.select("#chartContainer")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -12,6 +15,9 @@ const svg = d3.select("#chartContainer")
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Create the tooltip
+const tooltip = d3.select("#tooltip");
+
 // Load and parse the CSV data
 d3.csv("../data/data.csv").then(data => {
     const parseYear = d3.timeParse("%Y");
@@ -19,10 +25,11 @@ d3.csv("../data/data.csv").then(data => {
     data.forEach(d => {
         d.Year = parseYear(d.Year);
     });
-    console.log(data)
 
     // Create series from the data
     const series = createSeries(data);
+
+    console.log(data)
 
     // Create chart with the series
     drawChart(data, series);
@@ -44,6 +51,7 @@ function createSeries(data) {
             }))
         };
     });
+    console.log(series)
 
     return series;
 }
@@ -53,7 +61,7 @@ function drawChart(data, series) {
     const xDomain = d3.extent(series[0].values, d => d.date);
 
     const yDomain = [
-        d3.min(series, s => d3.min(s.values, d => d.value)),
+        0,
         d3.max(series, s => d3.max(s.values, d => d.value))
     ];
 
@@ -71,25 +79,42 @@ function drawChart(data, series) {
 
     // Add the title
     svg.append("text")
-        .attr("x", 0)
+        .attr("x", -50)
         .attr("y", -20)
         .attr("text-anchor", "start")
-        .attr("class", "chart-title font-poppins text-2x1")
+        .attr("class", "chart-title font-poppins text-1xl text-gray-800")
         .text("GDP per Capita (2022 CAD)");
 
+    // Ensure max values on x-axis are always displayed
+    const defaultXTicks = xScale.ticks();
+    if (!defaultXTicks.includes(xDomain[1])) {
+        defaultXTicks.push(xDomain[1])
+        defaultXTicks.splice((defaultXTicks.length - 1) - 1, 1);
+    }
+    
     // Add the axes
-    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
-    const yAxis = d3.axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale)
+        .tickValues(defaultXTicks)
+        .tickFormat(d3.timeFormat("%Y"));
+
+    const yAxis = d3.axisLeft(yScale)
+        .tickFormat(currencyFormat);
 
     svg.append("g")
-        .attr("class", "x axis")
+        .attr("class", "x axis font-poppins text-gray-800")
         .attr("transform", `translate(0,${height})`)
-        .call(xAxis);
+        .call(xAxis)
+        .select(".domain, .tick line")
+        .attr("class", "stroke-current text-gray-800");
 
     svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
+        .attr("class", "y axis font-poppins text-gray-800")
+        .call(yAxis)
+        .select(".domain")
+        .style("stroke-opacity", 0)
+        .select(".domain, .tick line")
+        .attr("class", "stroke-current text-gray-800");
+        
     // Modify the line generator to handle missing points DOESNT WORK YET
     const lineGenerator = d3.line()
         .defined(d => d.value !== 0)
@@ -104,5 +129,20 @@ function drawChart(data, series) {
             .attr("stroke", colorScale(index))
             .attr("stroke-width", 1.5)
             .attr("d", lineGenerator);
+    });
+
+    // Create invisible circles for each tooltip DOESN'T WORK YET
+    series.forEach((s, index) => {
+        svg.selectAll(`.circle-tooltip-${index}`)
+            .data(s.values)
+            .join("circle")
+            .attr("class", `circle-tooltip-${index}`)
+            .attr("cx", d => xScale(d.date))
+            .attr("cy", d => yScale(d.value))
+            .attr("r", 5) // Adjust radius as needed
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .append("title")
+            .text(s.name)
     });
 }
